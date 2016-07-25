e3fb5dfafdfd96c11cefb6090be13a64d04f0f4f
martin-sicho <sicho.martin@gmail.com>
2016-07-25 15:15:32 +0200

    added a few more fingerprints

--- desc_calculators.py
+++ desc_calculators.py
@@ -3,6 +3,7 @@ import os
 from rdkit import RDConfig
 from rdkit import Chem
 from rdkit.Chem import ChemicalFeatures
+from rdkit.Chem import MACCSkeys
 from rdkit.Chem.Pharm2D import Generate
 from rdkit.Chem.Pharm2D.SigFactory import SigFactory
 
@@ -23,3 +24,15 @@ def compute_fg_2D_pharm(smiles):
 
     fingerprints = np.array(fingerprints)
     return fingerprints, ['bit_{0}'.format(x) for x in range(len(fingerprints[0]))]
+
+def compute_fg_2D_topo(smiles):
+    mols = [Chem.MolFromSmiles(smile) for smile in smiles]
+    fps = [Chem.rdmolops.RDKFingerprint(mol) for mol in mols]
+    fps = np.array(fps)
+    return fps, ['bit_{0}'.format(x) for x in range(len(fps[0]))]
+
+def compute_fg_MACCS(smiles):
+    mols = [Chem.MolFromSmiles(smile) for smile in smiles]
+    fps = [MACCSkeys.GenMACCSKeys(mol) for mol in mols]
+    fps = np.array(fps)
+    return fps, ['bit_{0}'.format(x) for x in range(len(fps[0]))]
