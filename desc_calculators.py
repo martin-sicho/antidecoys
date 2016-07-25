832d7cb46cd40f13fb33230581209169bb1154f4
martin-sicho <sicho.martin@gmail.com>
2016-07-25 16:11:49 +0200

    added morgan fingerprints

--- desc_calculators.py
+++ desc_calculators.py
@@ -2,6 +2,7 @@ import os
 
 from rdkit import RDConfig
 from rdkit import Chem
+from rdkit.Chem import AllChem
 from rdkit.Chem import ChemicalFeatures
 from rdkit.Chem import MACCSkeys
 from rdkit.Chem.Pharm2D import Generate
@@ -34,5 +35,11 @@ def compute_fg_2D_topo(smiles):
 def compute_fg_MACCS(smiles):
     mols = [Chem.MolFromSmiles(smile) for smile in smiles]
     fps = [MACCSkeys.GenMACCSKeys(mol) for mol in mols]
+    fps = np.array(fps)
+    return fps, ['bit_{0}'.format(x) for x in range(len(fps[0]))]
+
+def compute_fg_morgan(smiles):
+    mols = [Chem.MolFromSmiles(smile) for smile in smiles]
+    fps = [AllChem.GetMorganFingerprint(mol, 2) for mol in mols]
     fps = np.array(fps)
     return fps, ['bit_{0}'.format(x) for x in range(len(fps[0]))]
